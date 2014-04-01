b989223d513fee39a80c58951cd72261d5a9d431
Mike Jarrett <mike.jarrett@hogarthww.com>
2014-04-01 16:45:34 +0100

    Add versioning to __init__.py

--- setup.py
+++ setup.py
@@ -4,13 +4,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import pipcheck
+
 config = {
     'description': 'Environment package update checker',
     'author': 'Mike Jarrett',
     'url': 'https://github.com/mikejarrett',
     'download_url': 'https://github.com/mikejarrett',
     'author_email': 'mdj00m@gmail.com',
-    'version': '0.1a',
+    'version': pipcheck.__version__,
     'install_requires': ['nose'],
     'packages': ['pipcheck'],
     'scripts': [],

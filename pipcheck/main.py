77947fbaaecdf973d062967e6cb07c7c4fff989f
Mike Jarrett <mike.jarrett@hogarthww.com>
2014-04-01 16:45:53 +0100

    Require at least one file name and path

--- pipcheck/main.py
+++ pipcheck/main.py
@@ -8,15 +8,14 @@ def main():
     parser = argparse.ArgumentParser(
         description='pipcheck checks the pip install packages of your '
         'environment for updates')
-    parser.add_argument('-c', '--csv', nargs='?', metavar='path/FILE',
+    parser.add_argument('-c', '--csv', nargs='?', metavar='/path/file',
                         help='Define a location for csv output')
     parser.add_argument(
-        '-r', '--requirements', metavar='path/FILE',
+        '-r', '--requirements', metavar='/path/file',
         help='Define location for new requirements file output')
 
     args = parser.parse_args()
+    if not (args.csv or args.requirements):
+        parser.error('Need at least --csv or --requirements')
+
     Checker(csv_file=args.csv, new_config=args.requirements)()
-
-
-if __name__ == '__main__':
-    main()

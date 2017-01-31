c3d6ae059b5ec77136a5a93c2f8ed887c9cf6f35
Mike Jarrett <mike.d.jarrett@gmail.com>
2017-01-31 19:33:35 +0000

    Add a simple publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,47 @@
 #! /bin/bash
+
+show_help () {
+	echo "Simple publishing script for Python projects"
+	echo
+	echo "usage: ./publish.sh [-h] [-v=VERSION] [-p]"
+	echo "optional arguments:"
+	echo "-h, --help   		Show this help message and exit"
+	echo "-v, --version		Update to version and commit"
+	echo "-p, --publish		If CI passes, publish to PyPI."
+}
+
+bumpversion_and_commit() {
+	sed -ri "s/(__version__ = ')(.*)'/\1$VERSION'/g" pipcheck/__init__.py
+    git add pipcheck/__init__.py
+    git commit -m "Bump version to $VERSION"
+
+	git ls-files --other --error-unmatch . >/dev/null 2>&1; ec=$?
+	if [ "$ec" = 0 ] ; then
+		echo "There are some untracked files. Not publishing."
+        exit 1
+	elif [ "$ec" = 1 ]; then
+		echo "No untracked files"
+	else
+		echo "Received an error from ls-files"
+		exit 1
+	fi
+}
+
+export PUBLISH=0
+export VERSION=
+
+while true ; do
+  case "$1" in
+    -h) show_help ; exit 0 ;;
+	-v|--version) export VERSION="$2" ; shift 2 ;;
+    -p|--publish) export PUBLISH="1" ; shift 1 ;;
+    *) break ;;
+  esac
+done
+
+echo "Bumping to version $VERSION"
+echo "Publishing $PUBLISH"
+
 echo "Running tests..."
 if tox ; then
     echo ''
@@ -35,11 +78,17 @@ else
     rm -f pycodestyle.txt
 fi
 
-python setup.py sdist upload -r pypi
+if [ ! -z "$VERSION" ] ; then
+	bumpversion_and_commit
+fi
 
-if [ $? -eq 0 ] ; then
-    echo "All done!"
+if [ "$PUBLISH" = "1" ] ; then
+    python setup.py sdist upload -r pypi
 
-else
-    echo "Uh-oh! :( :( :("
+    if [ $? -eq 0 ] ; then
+        echo "All done!"
+
+    else
+        echo "Error from sdist upload"
+    fi
 fi
